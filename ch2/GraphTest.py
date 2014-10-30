1d262420946916e89047c14a4cfd4c23eafec7e5
exiledkingcc <exiledkingcc@gmail.com>
2014-10-30 21:37:21 +0800

    2-4

--- ch2/GraphTest.py
+++ ch2/GraphTest.py
@@ -4,6 +4,7 @@ import string
 from Graph import Vertex
 from Graph import Edge
 from Graph import Graph
+from RandomGraph import RandomGraph
 from GraphWorld import GraphWorld
 from GraphWorld import CircleLayout
 
@@ -25,9 +26,27 @@ def main(script, n='10', k='3', *args):
     gw.show_graph(g, layout)
     gw.mainloop()
 
+def main2(script, n='10', p='0.5', *args):
+
+    # create n Vertices
+    n = int(n)
+    p = float(p)
+    labels = string.ascii_lowercase + string.ascii_uppercase
+    vs = [Vertex(c) for c in labels[:n]]
+
+    # create a graph and a layout
+    g = RandomGraph(vs)
+    g.add_random_edges(p)
+    layout = CircleLayout(g)
+
+    # draw the graph
+    gw = GraphWorld()
+    gw.show_graph(g, layout)
+    gw.mainloop()
+
 
 if __name__ == '__main__':
     import sys
-    main(*sys.argv)
+    main2(*sys.argv)
 
 
